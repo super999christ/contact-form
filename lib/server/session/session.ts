da04f919e32817cc97a1a61d5b36caec79febf75
Cody Christ <cchrist1210@gmail.com>
2023-12-27 14:22:54 -0800

    Added session/cookie for SSO

---
+++ lib/server/session/session.ts
@@ -0,0 +1,55 @@
+import 'server-only';
+
+import type { IronSessionData } from 'iron-session';
+import { cookies } from 'next/headers';
+
+import {
+  getIronSession,
+  getServerActionIronSession,
+  type IronSessionOptions
+} from '.';
+
+export const sessionOptions: IronSessionOptions = {
+  password: process.env.COOKIE_SECRET as string,
+  cookieName: 'iron-session/pickleball/sso',
+  cookieOptions: {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== 'development',
+    domain: process.env.COOKIE_DOMAIN
+  }
+};
+
+declare module 'iron-session' {
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  interface IronSessionData {
+    user?: {
+      email: string;
+      isSuperAdmin: boolean;
+      uuid: string;
+      token: string;
+      expiration: string;
+      isCompleted: boolean;
+      oltToken: string;
+      pbUuid: string;
+    };
+  }
+}
+
+const getSession = async (req: Request, res: Response) => {
+  const session = await getIronSession<IronSessionData>(
+    req,
+    res,
+    sessionOptions
+  );
+  return session;
+};
+
+const getServerActionSession = async () => {
+  const session = await getServerActionIronSession<IronSessionData>(
+    sessionOptions,
+    cookies()
+  );
+  return session;
+};
+
+export { getServerActionSession, getSession };
