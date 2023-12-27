094e2432a5f2a51f687f8bd8ccb0b32999437c42
Cody Christ <cchrist1210@gmail.com>
2023-12-27 15:35:46 -0800

    Attached user information on server-side page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import { headers } from 'next/headers';
 import PickleballContactForm from '@lib/components/PageForms/PickleballContactForm';
 import { extractIP } from '@lib/utils/location';
+import { getServerActionSession } from '@lib/server/session/session';
 
 export default async function ContactFormPage() {
   const ip = extractIP(headers());
+  const session = await getServerActionSession();
+  const { user } = session;
+
   return <PickleballContactForm ip={ip} />;
 }
