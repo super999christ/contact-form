1c94d11fc944f448819331b6d1b9152f7927b692
Cody Christ <cchrist1210@gmail.com>
2023-12-27 14:26:45 -0800

    Added COOKIE_SECRET environment variable

--- lib/server/environment.ts
+++ lib/server/environment.ts
@@ -1,5 +1,6 @@
 export const Environment = {
   API_URL: String(process.env.API_URL),
   API_KEY: String(process.env.API_KEY),
-  RECAPTCHA_SECRET_KEY: String(process.env.RECAPTCHA_SECRET_KEY)
+  RECAPTCHA_SECRET_KEY: String(process.env.RECAPTCHA_SECRET_KEY),
+  COOKIE_SECRET: String(process.env.COOKIE_SECRET)
 };
